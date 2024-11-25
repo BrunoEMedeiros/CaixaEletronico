424e8244d463420a318547df4ec0134d6bda1cd5
D3adSec <bruno.emedeiros99@gmail.com>
2024-11-25 11:11:04 -0300

    add home page

--- front_end/JS/index.js
+++ front_end/JS/index.js
@@ -8,8 +8,10 @@ botao.addEventListener('click',
          let resposta = await fetch(`http://localhost:3000/login/${usuario}/${senha}`)
          if(resposta.status == 200)
          {
-            let mensagem = await resposta.json();
-            alert('Bem vindo!')
+            // replace é usado quando queremos redirecionar o
+            // usuario sem deixar a opção de navegar de volta para a tela anterior
+            window.location.replace("./home.html")
+            
          }
          else{
             alert('Usuario ou senha incorretos!')
